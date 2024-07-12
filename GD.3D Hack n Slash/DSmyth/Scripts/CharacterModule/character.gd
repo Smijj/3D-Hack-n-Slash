7844e9549358b74132909af9111d8fb6f5a19090
Declan Smyth <declan.smyth@hotmail.com>
2024-07-12 17:37:32 +0930

    Orgainsed player movement vars

--- GD.3D Hack n Slash/DSmyth/Scripts/CharacterModule/character.gd
+++ GD.3D Hack n Slash/DSmyth/Scripts/CharacterModule/character.gd
@@ -1,23 +1,22 @@
 class_name Character
 extends CharacterBody3D
 
+@export_category("Settings")
+@export var MoveSpeed = 5.0
+@export var JumpVelocity = 4.5
+
+@export_category("References")
 @export var StatsComp : StatsComponent
 @export var AttackComp : AttackComponent
 @export var DamageableComps : Array[DamageableComponent]
 
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
 func _ready():
-	
+	if !StatsComp or DamageableComps.size() == 0: return
 	for comp in DamageableComps:
 		comp.OnDamageTaken.connect(StatsComp.TakeDamage)
-		pass
-
-	pass
 
 func _physics_process(delta):
 	HandleMovement(delta)
@@ -30,21 +29,18 @@ func HandleMovement(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JumpVelocity
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var inputDir = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBack")
 	var direction = (transform.basis * Vector3(inputDir.x, 0, inputDir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * MoveSpeed
+		velocity.z = direction.z * MoveSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, MoveSpeed)
+		velocity.z = move_toward(velocity.z, 0, MoveSpeed)
 
-func Attack():
-	pass
 
 func Die():
 	pass
